34c9b550b84667ab5bed5cd93d426bca5c94d69e
Irakli Svanidze <svanidzeee@gmail.com>
2023-07-20 19:30:31 +0300

    connect font awesome library, import all icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Center, Flex, Button } from '@chakra-ui/react';
+import { findIconDefinition, library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as Icons from '@fortawesome/free-solid-svg-icons';
+
+// transform object to array and format
+const iconList = Object.keys(Icons)
+  .filter((key) => key !== 'fas' && key !== 'prefix')
+  .map((icon) => Icons[icon]);
+
+// add all icons
+library.add(...iconList);
+console.log(iconList);
+
+const coffeeLookup = { prefix: 'fas', iconName: iconList[0].iconName };
+const coffeeIconDefinition = findIconDefinition(coffeeLookup);
+
 function App() {
-  return <div>1</div>;
+  return (
+    <Flex width={'100vw'} height={'100vh'} alignContent={'center'} justifyContent={'center'}>
+      <Center>
+        <FontAwesomeIcon icon={coffeeIconDefinition} />
+        <Button>Change Icon</Button>
+      </Center>
+    </Flex>
+  );
 }
 
 export default App;
